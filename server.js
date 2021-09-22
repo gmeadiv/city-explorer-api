3de5b5ac0f543899153a1a1230e88b4d7982e4f3
George Mead <gmeadiv@gmail.com>
2021-09-21 18:35:48 -0700

    weather data access, forecast object done; object array still needed

--- server.js
+++ server.js
@@ -3,11 +3,9 @@
 require('dotenv').config();
 
 const express = require('express');
-const data = require('./data.json');
 const cors = require('cors');
-
+const forecast = require('./weather.json');
 const PORT = process.env.PORT || 3002;
-
 const app = express();
 
 app.use(cors());
@@ -16,13 +14,44 @@ app.get('/', (request, response) => {
   response.status(200).send('you made it!')
 });
 
-app.get('/data', (request, response) => {
+function Forecast(name, dayOne, dayTwo, dayThree) {
+  this.name = name;
+  this.descriptionOne = dayOne[0];
+  this.dateOne = dayOne[1];
 
-  const veg = request.query.veg === true;
+  this.descriptionTwo = dayTwo[0];
+  this.dateTwo = dayTwo[1];
 
-  const veggies = data.find(food => food.veg === true);
+  this.descriptionThree = dayThree[0];
+  this.dateThree = dayThree[1];
 
-  response.send(veggies)
+  console.log(this, '<---- OBJECT FUNCTION LOG ---<<<');
+  Forecast.forecastArray.push(this);
+};
+
+Forecast.forecastArray = [];
+console.log(Forecast.forecastArray, '<---- ARRAY LOG ---<<<');
+
+app.get('/forecast', (request, response) => {
+  // let searchQuery = request.query;
+  let searchQuery;
+  const cityInfo = forecast.find(city => city.searchQuery === searchQuery);
+
+  const dayOne = [];
+  const dayTwo = [];
+  const dayThree = [];
+
+  dayOne.push(cityInfo.data[0].weather.description, cityInfo.data[0].valid_date);
+  dayTwo.push(cityInfo.data[1].weather.description, cityInfo.data[1].valid_date);
+  dayThree.push(cityInfo.data[2].weather.description, cityInfo.data[2].valid_date);
+
+  // console.log(dayOne, dayTwo, dayThree, '<--- DAY ARRAYS LOG ---<<<');
+  
+  response.send(cityInfo.city_name);
+
+  const city = new Forecast(cityInfo.city_name, dayOne, dayTwo, dayThree);
+  
+  console.log(city, '<--- CITY OBJECT LOG ---<<<');
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
