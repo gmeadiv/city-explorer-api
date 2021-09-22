6c0cb3a22f0b6734e50c47f53cef61929864ee97
George Mead <gmeadiv@gmail.com>
2021-09-21 19:25:54 -0700

    tinkering with server.js

--- server.js
+++ server.js
@@ -14,44 +14,32 @@ app.get('/', (request, response) => {
   response.status(200).send('you made it!')
 });
 
-function Forecast(name, dayOne, dayTwo, dayThree) {
+function Forecast(name, description, date) {
   this.name = name;
-  this.descriptionOne = dayOne[0];
-  this.dateOne = dayOne[1];
+  this.description = description;
+  this.date = date;
 
-  this.descriptionTwo = dayTwo[0];
-  this.dateTwo = dayTwo[1];
-
-  this.descriptionThree = dayThree[0];
-  this.dateThree = dayThree[1];
-
-  console.log(this, '<---- OBJECT FUNCTION LOG ---<<<');
+  // console.log(this, '<---- OBJECT FUNCTION LOG ---<<<');
   Forecast.forecastArray.push(this);
+  console.log(Forecast.forecastArray, '<---- ARRAY OBJECT LOG ---<<<');
 };
 
 Forecast.forecastArray = [];
-console.log(Forecast.forecastArray, '<---- ARRAY LOG ---<<<');
 
 app.get('/forecast', (request, response) => {
   // let searchQuery = request.query;
   let searchQuery;
   const cityInfo = forecast.find(city => city.searchQuery === searchQuery);
-
-  const dayOne = [];
-  const dayTwo = [];
-  const dayThree = [];
-
-  dayOne.push(cityInfo.data[0].weather.description, cityInfo.data[0].valid_date);
-  dayTwo.push(cityInfo.data[1].weather.description, cityInfo.data[1].valid_date);
-  dayThree.push(cityInfo.data[2].weather.description, cityInfo.data[2].valid_date);
-
-  // console.log(dayOne, dayTwo, dayThree, '<--- DAY ARRAYS LOG ---<<<');
   
   response.send(cityInfo.city_name);
 
-  const city = new Forecast(cityInfo.city_name, dayOne, dayTwo, dayThree);
+  const dayOneForecast = new Forecast(cityInfo.city_name, cityInfo.data[0].weather.description, cityInfo.data[0].valid_date);
+  const dayTwoForecast = new Forecast(cityInfo.city_name, cityInfo.data[1].weather.description, cityInfo.data[1].valid_date);
+  const dayThreeForecast = new Forecast(cityInfo.city_name, cityInfo.data[2].weather.description, cityInfo.data[2].valid_date);
   
-  console.log(city, '<--- CITY OBJECT LOG ---<<<');
+  // console.log(dayOneForecast, dayTwoForecast, dayThreeForecast, '<--- FUNCTION CALL LOG ---<<<');
 });
+
+console.log(Forecast.forecastArray, '<---- ARRAY GLOBAL LOG ---<<<');
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
