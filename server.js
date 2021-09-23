ebfdcfb51e5e084c0b3fa69a40186b44e3e5786a
George Mead <gmeadiv@gmail.com>
2021-09-22 20:54:18 -0700

    last refactor w riva

--- server.js
+++ server.js
@@ -14,20 +14,20 @@ app.get('/', (request, response) => {
   response.status(200).send('you made it!')
 });
 
-function Forecast(item) {
-  this.description = item.weather.description;
-  this.date = item.valid_date;
+function Forecast(date, description) {
+  this.description = description;
+  this.date = date;
 
   // console.log(this, '<---- OBJECT FUNCTION LOG ---<<<');
 };
 
 app.get('/forecast', (request, response) => {
-  let {searchQuery} = request.query;
+  let searchQuery = request.query.searchQuery;
     console.log(request.query, '<---- QUERY LOG ---<<<');
   const cityInfo = forecast.find(city => city.city_name === searchQuery);
 
   try{
-    const forecastArray = cityInfo.data.map(item => new Forecast(item));
+    const forecastArray = cityInfo.data.map(item => new Forecast(item.valid_date, item.weather.description));
     response.status(200).send(forecastArray);
   } catch(error) {
     console.log(error, 'ERROR LOG')
