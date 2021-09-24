557b2f9ade2e704131ed7d039e1f69446d5a1f48
George Mead <gmeadiv@gmail.com>
2021-09-23 20:24:18 -0700

    adds weather api, back-end is now fully functional

--- routes.js
+++ routes.js
@@ -5,37 +5,38 @@ function getHome(request, response) {
   response.status(200).send('Welcome to the Void')
 }
 
-// async function getForecast(request, response) {
-//   let searchQuery = request.query.searchQuery;
-//   response.status(200).send(request.query.searchQuery, '<---- SEARCH QUERY LOG ---<<<');
-//   const forecastURL = forecast.find(city => city.city_name === searchQuery);
+async function getForecast(request, response) {
+  let searchQuery = request.query.searchQuery;
+  let lat = request.query.latitude;
+  let lon = request.query.longitude;
 
-//   console.log(forecastURL, '<---- FORECASTURL LOG ---<<<');
+  console.log(searchQuery, '<---- SEARCH QUERY LOG');
+  
+  const forecastURL = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
 
-//   https://api.weatherbit.io/v2.0/current?lat=31.95&lon=35.91&key=58ed32f63be44a96927ed628f2284360%09
+  // console.log(forecastURL, '<---- FORECAST URL LOG ---<<<');
 
-//   try{
-//     const forecastArray = cityInfo.data.map(item => new Forecast(item.valid_date, item.weather.description));
-//     response.status(200).send(forecastArray);
-//   } catch(error) {
-//     console.log(error, '<---- ERROR LOG ---<<<')
-//   }
-// }
+  const forecastResponse = await axios.get(forecastURL);
+
+  // console.log(forecastResponse, '<---- FORECAST RESPONSE LOG ---<<<');
+
+  response.status(200).send(forecastResponse.data);
+}
 
 async function getMovies(request, response) {
   let {searchQuery} = request.query; 
 
-  console.log(searchQuery, '<---- MOVIES searchQuery LOG ---<<<')
+  // console.log(searchQuery, '<---- MOVIES searchQuery LOG ---<<<')
 
   const moviesURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${searchQuery}&page=1`;
 
-  console.log(moviesURL, '<---- MOVIES URL LOG ---<<<');
+  // console.log(moviesURL, '<---- MOVIES URL LOG ---<<<');
 
   const moviesResponse = await axios.get(moviesURL);
 
-  console.log(moviesResponse.data, '<---- MOVIES RESPONSE LOG ---<<<')
+  // console.log(moviesResponse.data, '<---- MOVIES RESPONSE LOG ---<<<')
 
   response.status(200).send(moviesResponse.data.results);
 }
 
-module.exports = {getHome, getMovies}
+module.exports = {getHome, getForecast, getMovies}
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-// const forecast = require('./weather.json');
 const handlerFunctions = require('./routes.js');
 
 const PORT = process.env.PORT;
@@ -14,12 +13,7 @@ app.use(cors());
 
 app.get('/', handlerFunctions.getHome);
 
-// function Forecast(date, description) {
-//   this.description = description;
-//   this.date = date;
-// };
-
-// app.get('/forecast', handlerFunctions.getForecast);
+app.get('/forecast', handlerFunctions.getForecast);
 
 app.get('/movies', handlerFunctions.getMovies);
 
