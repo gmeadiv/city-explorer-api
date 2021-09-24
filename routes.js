659cf12853fcb37a9c8e0528d64a2aa2a37c7a59
George Mead <gmeadiv@gmail.com>
2021-09-23 18:25:55 -0700

    adds operational movie api call

--- routes.js
+++ routes.js
@@ -1,13 +1,41 @@
 const axios = require('axios');
 
-async function handleGetMovies(req, res) {
-  // let {searchQuery} = req.query; 
-  // const moviesURL = `https://api.themoviedb.org/3/search/movie?key=${process.env.TMDB_API_KEY}&language=en-US&query=${searchQuery}&page=1&include_adult=false`;
-  // const moviesResponse = await axios.get(moviesURL);
-  // console.log(moviesResponse, '<---- MOVIES RESPONSE LOG ---<<<')
-
-
-  res.status(200).send('YOU MADE IT');
+function getHome(request, response) {
+  console.log('YOU ARE HOME');
+  response.status(200).send('Welcome to the Void')
 }
 
-module.exports = {handleGetMovies}
+// async function getForecast(request, response) {
+//   let searchQuery = request.query.searchQuery;
+//   response.status(200).send(request.query.searchQuery, '<---- SEARCH QUERY LOG ---<<<');
+//   const forecastURL = forecast.find(city => city.city_name === searchQuery);
+
+//   console.log(forecastURL, '<---- FORECASTURL LOG ---<<<');
+
+//   https://api.weatherbit.io/v2.0/current?lat=31.95&lon=35.91&key=58ed32f63be44a96927ed628f2284360%09
+
+//   try{
+//     const forecastArray = cityInfo.data.map(item => new Forecast(item.valid_date, item.weather.description));
+//     response.status(200).send(forecastArray);
+//   } catch(error) {
+//     console.log(error, '<---- ERROR LOG ---<<<')
+//   }
+// }
+
+async function getMovies(request, response) {
+  let {searchQuery} = request.query; 
+
+  console.log(searchQuery, '<---- MOVIES searchQuery LOG ---<<<')
+
+  const moviesURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${searchQuery}&page=1`;
+
+  console.log(moviesURL, '<---- MOVIES URL LOG ---<<<');
+
+  const moviesResponse = await axios.get(moviesURL);
+
+  console.log(moviesResponse.data, '<---- MOVIES RESPONSE LOG ---<<<')
+
+  response.status(200).send(moviesResponse.data.results);
+}
+
+module.exports = {getHome, getMovies}
--- server.js
+++ server.js
@@ -1,41 +1,26 @@
 'use strict';
 
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const forecast = require('./weather.json');
-const handlerFunctions = require('./routes')
-const PORT = process.env.PORT || 3002;
+// const forecast = require('./weather.json');
+const handlerFunctions = require('./routes.js');
+
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
 
-app.get('/', (request, response) => {
-  response.status(200).send('you made it!')
-});
+app.get('/', handlerFunctions.getHome);
 
-function Forecast(date, description) {
-  this.description = description;
-  this.date = date;
+// function Forecast(date, description) {
+//   this.description = description;
+//   this.date = date;
+// };
 
-  // console.log(this, '<---- CONSTRUCTOR FUNCTION LOG ---<<<');
-};
+// app.get('/forecast', handlerFunctions.getForecast);
 
-app.get('/forecast', (request, response) => {
-  let {searchQuery} = request.query;
-    console.log(request.query, '<---- QUERY LOG ---<<<');
-  const cityInfo = forecast.find(city => city.city_name === searchQuery);
+app.get('/movies', handlerFunctions.getMovies);
 
-  try{
-    const forecastArray = cityInfo.data.map(item => new Forecast(item.valid_date, item.weather.description));
-    response.status(200).send(forecastArray);
-  } catch(error) {
-    console.log(error, 'ERROR LOG')
-  }
-});
-
-app.get('/routes', handlerFunctions.handleGetMovies);
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => console.log('Listening on ' + PORT));
