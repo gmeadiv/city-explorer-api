6bdca85560f1cde2495833d1bc7dc5ec993ea8bc
George Mead <gmeadiv@gmail.com>
2021-09-24 18:19:00 -0700

    changes  cache from forecast to movies, doesn't store data tho

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-let cache = require('./cache.js')
+const cache = require('./cache.js')
 
 function getHome(request, response) {
   console.log('YOU ARE HOME');
@@ -11,11 +11,24 @@ async function getForecast(request, response) {
   let searchQuery = request.query.searchQuery;
   let lat = request.query.latitude;
   let lon = request.query.longitude;
-  const key = 'weather-' + lat +lon;
 
   console.log(searchQuery, '<---- FORECAST SEARCH QUERY LOG');
   
   const forecastURL = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
+  
+  const forecastResponse = await axios.get(forecastURL);
+
+  response.status(200).send(forecastResponse.data);
+}
+
+async function getMovies(request, response) {
+  let {searchQuery} = request.query;
+  const key = 'movies-' + {searchQuery};
+
+
+  console.log(searchQuery, '<---- MOVIES SEARCH QUERY LOG ---<<<')
+
+  const moviesURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${searchQuery}&page=1`;
 
   if (cache[key] && (Date.now() - cache[key].timestamp < 5000)) {
     console.log('--> CACHE HIT LOG <--')
@@ -23,39 +36,40 @@ async function getForecast(request, response) {
     console.log('--> CACHE MISS LOG <--');
     cache[key] = {};
     cache[key].timestamp = Date.now();
-    cache[key].data = await axios.get(forecastURL)
-    .then(response => parseWeather(response))
+    cache[key].data = await axios.get(moviesURL)
+    .then(response => parseMovies(response))
   }
-  
-  const forecastResponse = await axios.get(forecastURL);
 
-  response.status(200).send(forecastResponse.data);
+  // const moviesResponse = await axios.get(moviesURL);
 
-  return cache[key].data;
+  response.status(200).send(cache[key].data);
+
+  // console.log(cache[key].data, '<---- WHAT IS CACHE KEY DATA LOG ---<<<');
+  // return cache[key].data;
 }
 
-function parseWeather(weatherData) {
-  console.log(weatherData.data, '<---- WEATHER DATA LOG ---<<<')
-  // try {
-  //   const weatherSummaries = weatherData.data.map(day => {
-  //     return new Weather(day);
-  //   });
-  //   return Promise.resolve(weatherSummaries);
-  // } catch (event) {
-  //   return Promise.reject(event);
-  // }
+function parseMovies(movieData) {
+  try {
+    const movieSummaries = movieData.data.results.map(movie => {
+      return new Movie(movie);
+    });
+    return Promise.resolve(movieSummaries);
+  } catch (e) {
+    return Promise.reject(e);
+  } 
 }
 
-async function getMovies(request, response) {
-  let {searchQuery} = request.query; 
-
-  console.log(searchQuery, '<---- MOVIES searchQuery LOG ---<<<')
-
-  const moviesURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${searchQuery}&page=1`;
-
-  const moviesResponse = await axios.get(moviesURL);
-
-  response.status(200).send(moviesResponse.data.results);
+class Movie {
+  constructor(movie) {
+    // console.log(movie, '<---- WHAT IS PASSING INTO THE CONSTRUCTOR LOG ---<<<');
+    this.title = movie.original_title;
+    this.overview = movie.overview;
+    this.image = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
+    this.avgVote = movie.vote_average;
+    this.totalVotes = movie.vote_count;
+    this.popularity = movie.popularity;
+    this.releaseDate = movie.release_date;
+  }
 }
 
 module.exports = {getHome, getForecast, getMovies}
