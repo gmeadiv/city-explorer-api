61dcb9ee2c19473a6c446bf6dc29f023d8581f7a
George Mead <gmeadiv@gmail.com>
2021-09-24 17:03:11 -0700

    adds a cache that won't store data but does exist, rn things are rendering and not broken

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+let cache = require('./cache.js')
+
 function getHome(request, response) {
   console.log('YOU ARE HOME');
   response.status(200).send('Welcome to the Void')
@@ -9,14 +11,39 @@ async function getForecast(request, response) {
   let searchQuery = request.query.searchQuery;
   let lat = request.query.latitude;
   let lon = request.query.longitude;
+  const key = 'weather-' + lat +lon;
 
   console.log(searchQuery, '<---- FORECAST SEARCH QUERY LOG');
   
   const forecastURL = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
 
+  if (cache[key] && (Date.now() - cache[key].timestamp < 5000)) {
+    console.log('--> CACHE HIT LOG <--')
+  } else {
+    console.log('--> CACHE MISS LOG <--');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = await axios.get(forecastURL)
+    .then(response => parseWeather(response))
+  }
+  
   const forecastResponse = await axios.get(forecastURL);
 
   response.status(200).send(forecastResponse.data);
+
+  return cache[key].data;
+}
+
+function parseWeather(weatherData) {
+  console.log(weatherData, '<---- WEATHER DATA LOG ---<<<')
+  // try {
+  //   const weatherSummaries = weatherData.data.map(day => {
+  //     return new Weather(day);
+  //   });
+  //   return Promise.resolve(weatherSummaries);
+  // } catch (event) {
+  //   return Promise.reject(event);
+  // }
 }
 
 async function getMovies(request, response) {
